0ac6ee96eccddcce32935e02fb8501899b31b5d6
RGBKey <rgbkey@gmail.com>
2017-10-15 14:23:41 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# yolodice-verifier
+# bustadice-verifier
 
-A simple javascript website utilizing [forge](https://github.com/digitalbazaar/forge) crypto utilities to check bets [YOLOdice](https://yolodice.com/r?2FYJC-7JZ) for fairness
-
-Supports both manual entry and input of the CSV files the website provides.
+A simple javascript website utilizing [forge](https://github.com/digitalbazaar/forge) crypto utilities to check bets on [bustadice](https://bustadice.com) for fairness
 
 Also supports data entry via URL parameters (`server_seed`, `server_seed_hash`, `client_seed`, and `nonce`)
 
-Please note that provable fairness is only effective if you take note of the server seed hash before you start betting, and set your own client seeds.
-
-Please consider donating 1KW1yj5JJukm3JFS7H4nNyt5zQyzcKz9La
+Please consider donating 1L7jCdX32cRSLankubtGNWBedBn6M1NofJ
--- main.js
+++ main.js
@@ -4,28 +4,22 @@ let app = new Vue({
     router,
     el: '#app',
     data: {
-        MAX_ROLL: 999999,
-        SATOSHIS: 100000000,
         serverSeed: '',
         serverSeedHash: '',
         clientSeed: '',
-        nonce: 1,
-        verifiedRolls: []
+        nonce: 1
     },
     created: function() {
-        if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-            alert('The File APIs are not fully supported in this browser.');
-        }
         this.serverSeed = this.$route.query.server_seed || '';
         this.serverSeedHash = this.$route.query.server_seed_hash || '';
         this.clientSeed = this.$route.query.client_seed || '';
         this.nonce = this.$route.query.nonce || 1;
     },
     methods: {
-        hmacsha512: function(K, K2, n) {
+        hmacsha256: function(key, value) {
             let hash = forge.hmac.create();
-            hash.start('sha512', forge.util.hexToBytes(K));
-            hash.update(`${K2}.${n}`);
+            hash.start('sha256', key);
+            hash.update(value);
             return hash.digest().toHex();
         },
         sha256: function(K) {
@@ -33,72 +27,39 @@ let app = new Vue({
             md.update(K);
             return md.digest().toHex();
         },
-        hashToRoll: function(hash) {
-            for(let i = 0; i < 25; i++) {
-                let chunk = hash.substr(i * 5, 5);
-                let roll = parseInt(chunk, 16);
-                if(roll <= this.MAX_ROLL) {
-                    return {
-                        roll: roll,
-                        index: i * 5,
-                        chunk: chunk
-                    };
-                }
-            }
-            return {
-                roll: parseInt(hash.substr(125, 3), 16),
-                index: 125,
-                chunk: hash.substr(125, 3)
-            };
+        allInfoPresent: function() {
+            return this.splitSeeds(this.serverSeed)
+                && this.serverSeedHash === this.hashServerSeed(this.splitSeeds(this.serverSeed))
+                && !isNaN(this.nonce)
+                && this.nonce >= 1;
         },
-        htmlTemplateHash: function() {
-            let hash = this.hmacsha512(this.serverSeed, this.clientSeed, this.nonce);
-            let index = this.hashToRoll(hash).index;
-            let length = index < 125 ? 5 : 3;
-            // Please take note of the use of both substring and substr below
-            return `${hash.substring(0, index)}<strong>${hash.substr(index, length)}</strong>${hash.substring(index+length, 128)}`;
-        },
-        handleFileInput: function(files) {
-            let file = files[0];
-            let reader = new FileReader();
-            reader.onload = function() {
-                app.parseCsv(reader.result);
-            };
-            reader.readAsText(file);
-        },
-        parseCsv: function(data) {
-            this.verifiedRolls = [];
-            data = data.split('\n');
-            let header = data.shift().split(',');
-            this.serverSeedHash = header[1];
-            this.serverSeed = header[2];
-            this.clientSeed = header[3];
-            let noErrors = true;
-            for(let line of data) {
-                if(line.length !== 0 && line.split(',').length === 7) {
-                    line = line.split(',');
-                    let roll = {
-                        bet_id: line[0],
-                        nonce: parseInt(line[1]),
-                        roll: parseInt(line[2]),
-                        target: line[3],
-                        range: line[4],
-                        amount: parseFloat(line[5])/this.SATOSHIS,
-                        profit: parseFloat(line[6])/this.SATOSHIS,
-                        jackpot_profit: parseFloat(line[7])/this.SATOSHIS || 0
-                    };
-                    roll.verified = this.hashToRoll(this.hmacsha512(this.serverSeed, this.clientSeed, roll.nonce)).roll === roll.roll;
-                    this.verifiedRolls.push(roll);
-                }
+        splitSeeds: function(seed) {
+            const seeds = seed.split('_');
+            if(seeds.length === 2 && seeds[0].length === 64 && seeds[1].length === 64) {
+                return seeds;
+            } else {
+                return false;
             }
         },
-        allBetsVerify: function() {
-            for(let i = 0; i < this.verifiedRolls.length; i++) {
-                if(!this.verifiedRolls[i].verified) {
-                    return false;
-                }
-            }
-            return true;
+        hashServerSeed: function(seeds) {
+            let [ auditSeed, gameSeed ] = seeds;
+            const auditSeedHash = this.sha256(auditSeed);
+            const serverSeedHash = this.hmacsha256(gameSeed, auditSeedHash);
+            return serverSeedHash;
+        },
+        calculateGame: function(gameSeed, clientSeed, nonce, auditSeed) {
+            let hash = this.hmacsha256(`${gameSeed}|${clientSeed}|${nonce}`, auditSeed);
+            hash = hash.substr(0, 52/4);
+            const X = parseInt(hash, 16) / Math.pow(2, 52);
+            const multiplier = Math.floor(99 / (1 - X));
+            return Math.max(100, Math.min(multiplier, 100000000));
+        },
+        result: function() {
+            const seeds = this.splitSeeds(this.serverSeed);
+            let [ auditSeed, gameSeed ] = seeds;
+            const wagerHash = this.hmacsha256(auditSeed, this.nonce.toString());
+            const result = (this.calculateGame(gameSeed, this.clientSeed, this.nonce, wagerHash) / 100).toFixed(2) + 'x';
+            return result;
         }
     }
 });
